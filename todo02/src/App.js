f12f33777b9ff03529ea520ed7ae9a1b2f11e290
bpond37 <bpond37@gmail.com>
2019-10-23 16:08:01 +0900

    todolist v2 - use only react hooks
    no class function

---
+++ todo02/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Todo from './todo';
+
+
+function App() {
+
+  return (
+    <div className="App">
+     <Todo></Todo>
+    </div>
+  );
+}
+
+export default App;
